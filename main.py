6e8a2074aec1316c513af260076d24891797ac9e
RainierJared <116169371+RainierJared@users.noreply.github.com>
2024-10-27 16:27:36 +1300

    Update main.py
    Changed it to localhost instead of my IP

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from View.view import webServer
 
 if __name__ == '__main__':
     pwa = webServer('MLR',template_folder='./templates')
-    pwa.run(host='192.168.1.32', port='8000', debug=True)
+    pwa.run(host='localhost', port='8000', debug=True)
